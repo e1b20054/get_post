13316d793bc110b0558ce6cdfaa892b61bc77e37
e1b20023 <e1b20023@oit.ac.jp>
2022-10-04 14:23:12 +0900

    リポジトリの初期化

---
+++ src/main/java/mob/team9/get_post/GetPostApplication.java
@@ -0,0 +1,13 @@
+package mob.team9.get_post;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetPostApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GetPostApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mob/team9/get_post/GetPostApplicationTests.java
@@ -0,0 +1,13 @@
+package mob.team9.get_post;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GetPostApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
